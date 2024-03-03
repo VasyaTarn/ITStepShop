00902f4bc3a1e2f115a5e75a179c8f42fcd86334
VasyaTarn <vasatopktoneverittomuvlob@gmail.com>
2024-03-03 15:29:49 +0200

    Add project files.

---
+++ Controllers/UsageController.cs
@@ -0,0 +1,97 @@
+﻿using ITStepShop.Data;
+using ITStepShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITStepShop.Controllers
+{
+    [Authorize(Roles = $"{WC.AdminRole}")]
+    public class UsageController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UsageController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: UsageController
+        public ActionResult Index()
+        {
+            IEnumerable<Usage> usageList = _db.Usages;
+            return View(usageList);
+        }
+
+        // GET: UsageController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsageController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsageController/Create
+        [HttpPost]
+        public ActionResult Create(Usage usage)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Usages.Add(usage);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        // GET: UsageController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var usage = _db.Usages.Find(id);
+            if (usage == null) return NotFound();
+            return View(usage);
+        }
+
+        // POST: UsageController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Usage usage)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Usages.Update(usage);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usage);
+        }
+
+        // GET: UsageController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == null || id == 0) return NotFound();
+            var usage = _db.Usages.Find(id);
+            if (usage == null) return NotFound();
+            return View(usage);
+        }
+
+        // POST: UsageController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var usage = _db.Usages.Find(id);
+            if (usage == null) return NotFound();
+            else
+            {
+                _db.Usages.Remove(usage);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
---
+++ Migrations/20240228171110_AddUsage.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITStepShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsageId",
+                table: "Product",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Usages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_UsageId",
+                table: "Product",
+                column: "UsageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Usages_UsageId",
+                table: "Product",
+                column: "UsageId",
+                principalTable: "Usages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Usages_UsageId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Usages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_UsageId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "UsageId",
+                table: "Product");
+        }
+    }
+}
